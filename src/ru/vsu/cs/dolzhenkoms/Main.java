f38e1264ff01752248d0730f16d580ba1a4413e8
mADoTM <kon4ita228@gmail.com>
2021-09-19 22:54:25 +0300

    Remove 'fake' RC2 (Rectangle)

--- src/ru/vsu/cs/dolzhenkoms/Main.java
+++ src/ru/vsu/cs/dolzhenkoms/Main.java
@@ -12,7 +12,6 @@ public class Main {
     public static final HorizontalParabola HP1 = new HorizontalParabola(-2, -4, -0.25);
     public static final VerticalParabola VP1 = new VerticalParabola(4, -5, 0.25);
     public static final Rectangle RC1 = new Rectangle(new Point2D.Double(-5.0,-3.0), new Point2D.Double(3.0,4.0));
-    public static final Rectangle RC2 = new Rectangle(new Point2D.Double(0,3.0), new Point2D.Double(5.0,30.0));
 
     public enum SimpleColor {
         BLACK,
@@ -47,7 +46,7 @@ public class Main {
                 }
                 if(RC1.isPointInsideRectangle(x,y)) {
                     if(VP1.isPointInsideOfParabola(x,y)) {
-                        if(RC2.isPointInsideRectangle(x,y)) {
+                        if(x > 0 && y > 3) {
                             return SimpleColor.GRAY;
                         }
                         return SimpleColor.ORANGE;
@@ -56,14 +55,13 @@ public class Main {
                     }
                 }
                 if(VP1.isPointInsideOfParabola(x,y)) {
-                    if(RC2.isPointInsideRectangle(x,y)) {
-                        return SimpleColor.WHITE;
-                    } else {
-                        if(x < 0) {
-                            return SimpleColor.YELLOW;
-                        }
+                    if(x > 5) {
                         return SimpleColor.BLUE;
                     }
+                    if(x < 0) {
+                        return SimpleColor.YELLOW;
+                    }
+                    return SimpleColor.WHITE;
                 }
                 if(y > -1) {
                     return SimpleColor.WHITE;
@@ -74,7 +72,7 @@ public class Main {
                     if(RC1.isPointInsideRectangle(x,y)) {
                         return SimpleColor.BLUE;
                     }
-                    if(RC2.isPointInsideRectangle(x,y)) {
+                    if(x > 4 && x < 5 && y > 3 && y < 4) {
                         return SimpleColor.GREEN;
                     }
                     return SimpleColor.GRAY;
